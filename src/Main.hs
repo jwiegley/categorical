34e6eac13a7a799c2c5b5e87884738151e113601
John Wiegley <johnw@newartisans.com>
2017-04-10 17:34:25 -0700

    Comment out some code

--- src/Main.hs
+++ src/Main.hs
@@ -3,23 +3,26 @@
 {-# OPTIONS_GHC -Wall #-}
 
 {-# OPTIONS_GHC -fplugin=ConCat.Plugin #-}
+{-# OPTIONS_GHC -fsimpl-tick-factor=2800 #-}
+{-# OPTIONS_GHC -fexpose-all-unfoldings #-}
 
 module Main where
 
 import ConCat.AltCat (ccc)
 import ConCat.Syntactic (render)
-import Control.Arrow (Kleisli(..))
-import Control.Monad.Free
+-- import Control.Arrow (Kleisli(..))
+import Control.Monad
+-- import Control.Monad.Free
 
 import Categorical
 
 equation :: Num a => a -> a -> a
 equation x y = x - 3 + 7 * y
 
-type Teletype = Free TeletypeF
+-- type Teletype = Free TeletypeF
 
-tele :: Kleisli Teletype () ()
-tele = Kleisli $ \() -> Free $ Get $ \c -> Free $ Put c (Free $ Put c (Pure ()))
+-- tele :: Kleisli Teletype () ()
+-- tele = Kleisli $ \() -> Free $ Get $ \c -> Free $ Put c (Free $ Put c (Pure ()))
 
 main :: IO ()
 main = do

73628513b469b4e4a8baa53dbb5ee2bcf073d95e
John Wiegley <johnw@newartisans.com>
2017-04-20 23:07:43 -0700

    Change NonDet to a simpler type

--- src/Categorical/Types.hs
+++ src/Categorical/Types.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
@@ -6,6 +7,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PackageImports #-}
 {-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
@@ -28,13 +30,16 @@
 
 module Categorical.Types where
 
-import ConCat.Category
-import Control.Arrow (Kleisli(..))
-import Control.Monad.State
+import           ConCat.Category
+import           ConCat.Rep
+import           Control.Arrow (Kleisli(..))
+import           Control.Monad.State
 import "newtype" Control.Newtype (Newtype(..))
-import Data.Coerce
-import Prelude hiding ((.), id, curry, uncurry, const)
-import Z3.Category
+import           Data.Coerce
+import           Data.Functor.Identity
+import           Data.Monoid
+import           Prelude hiding ((.), id, curry, uncurry, const)
+import           Z3.Category
 
 data Position
     = V1
@@ -77,112 +82,107 @@ instance ProgramCat (->) Int where
 
 type Latency = Int
 
-instance Ok (Kleisli (State Latency)) Int => ProgramCat (Kleisli (State Latency)) Int where
-    xfer = Kleisli $ \a -> coerce a <$ modify (+ 5)
-    load = Kleisli $ \a -> load a   <$ modify (+ 10)
-    mov  = Kleisli $ \a -> mov a    <$ modify (+ 30)
-    add  = Kleisli $ \a -> add a    <$ modify (+ 50)
-    ret  = Kleisli $ \a -> ret a    <$ modify (+ 1)
+newtype NonDet p a b = NonDet { runNonDet :: p -> a -> (b, p) }
+    deriving Functor
 
-{-
-data NonDet k a b where
-    NonDet :: (EvalE p, GenE p) => (p -> k a b) -> NonDet k a b
+pattern N :: (p -> a -> (b, p)) -> NonDet p a b
+pattern N f = NonDet f
 
-deriving instance Show (NonDet k a b)
+instance Category (NonDet p) where
+    id = N (\p x -> (x, p))
+    N f . N g = N (\p x -> let (y, p') = g p x in f p' y)
 
-instance Category k => Category (NonDet k) where
-    type Ok (NonDet k) = Ok k
-    id = NonDet (\() -> id)
-    NonDet f . NonDet g = NonDet (\(p1, p2) -> f p1 . g p2)
+instance Monoid p => ProductCat (NonDet p) where
+    exl = N (\p x -> (exl x, p))
+    exr = N (\p x -> (exr x, p))
+    N f &&& N g = N $ \p x ->
+        let (f', fp) = f p x
+            (g', gp) = g p x in
+        ((f', g'), fp <> gp)
 
-instance (OpCon (Prod k) (Ok' k), ProductCat k) => ProductCat (NonDet k) where
-    exl = NonDet (\() -> exl)
-    exr = NonDet (\() -> exr)
-    NonDet f &&& NonDet g = NonDet (\(p1, p2) -> f p1 &&& g p2)
+instance TerminalCat (NonDet p) where
+    it = N (\p x -> (it x, p))
 
-instance TerminalCat k => TerminalCat (NonDet k) where
-    it = NonDet (\() -> it)
+instance ConstCat (NonDet p) b where
+    const b = N (\p x -> (const b x, p))
 
-instance ConstCat k b => ConstCat (NonDet k) b where
-    const b = NonDet (\() -> const b)
+instance BottomCat (NonDet p) a b where
+    bottomC = N (\p x -> (bottomC x, p))
 
-instance BottomCat k a b => BottomCat (NonDet k) a b where
-    bottomC = NonDet (\() -> bottomC)
+instance UnknownCat (NonDet p) a b where
+    unknownC = N (\p x -> (unknownC x, p))
 
-instance UnknownCat k a b => UnknownCat (NonDet k) a b where
-    unknownC = NonDet (\() -> unknownC)
+instance Monoid p => ClosedCat (NonDet p) where
+    -- curry   (NonDet (Kleisli f)) = N $ \x -> pure $ \y -> f (x, y)
+    curry   (NonDet f) = error "curry NYI"
+    uncurry (NonDet f) = N $ \p (x, y) -> let (f', p') = f p x in (f' y, p')
 
-instance ClosedCat k => ClosedCat (NonDet k) where
-    curry   (NonDet f) = NonDet (curry . f)
-    uncurry (NonDet f) = NonDet (uncurry . f)
+instance CoproductCat (NonDet p) where
+    inl = N (\p x -> (inl x, p))
+    inr = N (\p x -> (inr x, p))
+    N f ||| N g = error "(|||) NYI"
 
--- instance CoproductCat (NonDet k) where
---     inl = NonDet Left
---     inr = NonDet Right
---     NonDet f ||| NonDet g = NonDet (f ||| g)
+instance (Monoid p, Eq a) => EqCat (NonDet p) a where
+    equal    = N (\p x -> (equal x, p))
+    notEqual = N (\p x -> (notEqual x, p))
 
-instance (EqCat k a, Eq a) => EqCat (NonDet k) a where
-    equal    = NonDet (\((), ()) -> equal)
-    notEqual = NonDet (\((), ()) -> notEqual)
+instance (Monoid p, Ord a) => OrdCat (NonDet p) a where
+    lessThan           = N (\p x -> (lessThan x, p))
+    greaterThan        = N (\p x -> (greaterThan x, p))
+    lessThanOrEqual    = N (\p x -> (lessThanOrEqual x, p))
+    greaterThanOrEqual = N (\p x -> (greaterThanOrEqual x, p))
 
-instance (OrdCat k a, Ord a) => OrdCat (NonDet k) a where
-    lessThan           = NonDet (\() -> lessThan)
-    greaterThan        = NonDet (\() -> greaterThan)
-    lessThanOrEqual    = NonDet (\() -> lessThanOrEqual)
-    greaterThanOrEqual = NonDet (\() -> greaterThanOrEqual)
+-- instance Fractional a => FractionalCat (NonDet p) a where
+--     recipC = N recip
+--     divideC = N (uncurry (/))
 
--- instance Fractional a => FractionalCat (NonDet k) a where
---     recipC = NonDet recip
---     divideC = NonDet (uncurry (/))
+-- instance (RealFrac a, Integral b) => RealFracCat (NonDet p) a b where
+--     floorC = N floor
+--     ceilingC = N ceiling
 
--- instance (RealFrac a, Integral b) => RealFracCat (NonDet k) a b where
---     floorC = NonDet floor
---     ceilingC = NonDet ceiling
+-- instance Floating a => FloatingCat (NonDet p) a where
+--     expC = N exp
+--     cosC = N cos
+--     sinC = N sin
 
--- instance Floating a => FloatingCat (NonDet k) a where
---     expC = NonDet exp
---     cosC = NonDet cos
---     sinC = NonDet sin
+-- instance (Integral a, Num b) => FromIntegralCat (NonDet p) a b where
+--     fromIntegralC = N fromIntegral
 
--- instance (Integral a, Num b) => FromIntegralCat (NonDet k) a b where
---     fromIntegralC = NonDet fromIntegral
-
--- instance DistribCat (NonDet k) where
---     distl = NonDet $ \(x, e) -> case e of
+-- instance DistribCat (NonDet p) where
+--     distl = N $ \(x, e) -> case e of
 --         Left y  -> Left (x, y)
 --         Right z -> Right (x, z)
---     distr = NonDet $ \(e, x) -> case e of
+--     distr = N $ \(e, x) -> case e of
 --         Left y  -> Left (y, x)
 --         Right z -> Right (z, x)
 
--- instance (HasRep a, r ~ Rep a) => RepCat (NonDet k) a r where
---     reprC = NonDet repr
---     abstC = NonDet abst
+instance (HasRep a, r ~ Rep a) => RepCat (NonDet p) a r where
+    reprC = N (\p x -> (repr x, p))
+    abstC = N (\p x -> (abst x, p))
 
--- instance (Enum a, Show a) => EnumCat (NonDet k) a where
---     succC = NonDet succ
---     predC = NonDet pred
+instance (Enum a, Show a) => EnumCat (NonDet p) a where
+    succC = N (\p x -> (succ x, p))
+    predC = N (\p x -> (pred x, p))
 
-instance BoolCat k => BoolCat (NonDet k) where
-    notC = NonDet (\() -> notC)
-    andC = NonDet (\() -> andC)
-    orC  = NonDet (\() -> orC)
-    xorC = NonDet (\() -> xorC)
+instance Monoid p => BoolCat (NonDet p) where
+    notC = N (\p x -> (notC x, p))
+    andC = N (\p x -> (andC x, p))
+    orC  = N (\p x -> (orC x, p))
+    xorC = N (\p x -> (xorC x, p))
 
-instance IfCat k a => IfCat (NonDet k) a where
-    ifC = NonDet (\() -> ifC)
+instance Monoid p => IfCat (NonDet p) a where
+    ifC = N (\p x -> (ifC x, p))
 
-instance (NumCat k a, Num a) => NumCat (NonDet k) a where
-    negateC = NonDet (\() -> negateC)
-    addC    = NonDet (\() -> addC)
-    subC    = NonDet (\() -> subC)
-    mulC    = NonDet (\() -> mulC)
-    powIC   = NonDet (\() -> powIC)
+instance Num a => NumCat (NonDet p) a where
+    negateC = N (\p x -> (negateC x, p))
+    addC    = N (\p x -> (addC x, p))
+    subC    = N (\p x -> (subC x, p))
+    mulC    = N (\p x -> (mulC x, p))
+    powIC   = N (\p x -> (powIC x, p))
 
-instance ProgramCat k Int => ProgramCat (NonDet k) Int where
-    xfer = NonDet (\b -> if b then xfer else xfer)
-    load = NonDet (\b -> if b then load else load)
-    mov  = NonDet (\b -> if b then mov else mov)
-    add  = NonDet (\b -> if b then add else add)
-    ret  = NonDet (\b -> if b then ret else ret)
--}
+instance ProgramCat (NonDet Latency) Int where
+    xfer = N (\l x -> (if l < 10 then xfer x else xfer x, l + 5))
+    add  = N (\l x -> (if l < 10 then add  x else add  x, l + 50))
+    mov  = N (\l x -> (if l < 10 then mov  x else mov  x, l + 30))
+    load = N (\l x -> (if l < 10 then load x else load x, l + 10))
+    ret  = N (\l x -> (if l < 10 then ret  x else ret  x, l + 1))
--- test/Main.hs
+++ test/Main.hs
@@ -22,7 +22,7 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 
 {-# OPTIONS_GHC -fplugin=ConCat.Plugin #-}
-{-# OPTIONS_GHC -fplugin-opt=ConCat.Plugin:trace #-}
+-- {-# OPTIONS_GHC -fplugin-opt=ConCat.Plugin:trace #-}
 {-# OPTIONS_GHC -fsimpl-tick-factor=1000 #-}
 {-# OPTIONS_GHC -fexpose-all-unfoldings #-}
 
@@ -43,6 +43,7 @@ import ConCat.Category
 import ConCat.Syntactic (render)
 import Control.Arrow (Kleisli(..))
 import Control.Monad.State
+import Data.Functor.Identity
 import Functions
 import Prelude hiding ((.), id, curry, uncurry, const)
 import Z3.Category
@@ -77,16 +78,17 @@ main = do
 
     print $ ccc program (10, 20, 30)
 
-    print $ runState (runKleisli (ccc @(Kleisli (State Latency)) program)
+    print $ runState (runKleisli (runNonDet (ccc @(NonDet Latency) program))
                                  (10, 20, 30)) 0
 
     -- Ask Z3 to find a suitable program for us using not only existential
     -- degrees of freedom, but interactions between these degrees of freedom
     -- and whatever metadata resulted from earlier choices.
-    case ccc @(Kleisli (State Latency)) program of
-        Kleisli f -> do
-            let k s x y z = runState (f (x, y, z)) s
-            mres <- runZ3Show (ccc @Z3Cat k)
+    case ccc @(NonDet Latency) program of
+        NonDet (Kleisli f) -> do
+            let k x y z s = runState (f (x, y, z)) s
+                g = k 10 20 30
+            mres <- runZ3Show (ccc @Z3Cat g)
             case mres of
                 Nothing -> putStrLn "No solution!"
-                Just p  -> print $ k p 10 20 30
+                Just p  -> print $ g p

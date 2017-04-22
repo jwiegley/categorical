81af5efc0d51a2c675cf126898f17289aeb75917
John Wiegley <johnw@newartisans.com>
2017-04-22 13:27:39 -0700

    Build out the NonDet type over arbitrary categories

--- src/Categorical/Types.hs
+++ src/Categorical/Types.hs
@@ -10,7 +10,9 @@
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -23,12 +25,13 @@
 
 module Categorical.Types where
 
-import           ConCat.Category
-import           ConCat.Rep
-import           Control.Monad.State
-import "newtype" Control.Newtype (Newtype(..))
-import           Data.Coerce
-import           Prelude hiding ((.), id, curry, uncurry, const)
+import ConCat.Category
+import ConCat.Rep
+import Control.Monad.State
+-- import "newtype" Control.Newtype (Newtype(..))
+import Data.Coerce
+import Z3.Category
+import Prelude hiding ((.), id, curry, uncurry, const)
 
 data Position
     = V1
@@ -36,30 +39,29 @@ data Position
     | V3
     deriving (Eq, Ord, Enum, Show, Read)
 
-newtype V (l :: Position) v = V v
-    deriving (Eq, Ord, Show, Read)
+type V (l :: Position) v = v
 
-instance Newtype (V l v) v where
-  pack = V
-  unpack (V v) = v
+-- instance Newtype (V l v) v where
+--     pack = V
+--     unpack (V v) = v
 
-instance CoerceCat (->) (V l1 v) (V l2 v) where
-    coerceC = coerce
+-- instance CoerceCat (->) (V l1 v) (V l2 v) where
+--     coerceC = coerce
 
-instance CoerceCat (NonDet p) (V l1 v) (V l2 v) where
-    coerceC = N $ \p a -> (coerce a, p)
+-- instance CoerceCat (NonDet p) (V l1 v) (V l2 v) where
+--     coerceC = N $ \p a -> (coerce a, p)
 
-instance CoerceCat (->) (V l v) v where
-    coerceC (V v) = v
+-- instance CoerceCat (->) (V l v) v where
+--     coerceC (V v) = v
 
-instance CoerceCat (->) v (V l v) where
-    coerceC = V
+-- instance CoerceCat (->) v (V l v) where
+--     coerceC = V
 
-instance CoerceCat (NonDet p) (V l v) v where
-    coerceC = NonDet $ \p (V v) -> (v, p)
+-- instance CoerceCat (NonDet p) (V l v) v where
+--     coerceC = NonDet $ \p (V v) -> (v, p)
 
-instance CoerceCat (NonDet p) v (V l v) where
-    coerceC = NonDet $ \p v -> (V v, p)
+-- instance CoerceCat (NonDet p) v (V l v) where
+--     coerceC = NonDet $ \p v -> (V v, p)
 
 class Ok k v => ProgramCat k v where
     xfer :: forall s t.           V s v `k` V t v
@@ -70,112 +72,107 @@ class Ok k v => ProgramCat k v where
 
 instance ProgramCat (->) Int where
     xfer            = coerce
-    load            = V
-    mov (V _x, V y) = V y
-    add (V x, V y)  = V (x + y)
-    ret (V x)       = x
+    load            = id
+    mov (_x, y) = y
+    add (x, y)  = x + y
+    ret x       = x
 
-newtype NonDet p a b = NonDet { runNonDet :: p -> a -> (b, p) }
-    deriving Functor
+data NonDet k a b where
+    NonDet :: (EvalE p, GenE p, Show p) => (p -> a `k` b) -> NonDet k a b
 
-pattern N :: (p -> a -> (b, p)) -> NonDet p a b
-pattern N f = NonDet f
+runNonDet :: NonDet k a b -> (forall p. (p -> a `k` b) -> a `k` b) -> a `k` b
+runNonDet (NonDet f) k = k f
 
-instance Category (NonDet p) where
-    id = N (\p x -> (x, p))
-    N f . N g = N (\p x -> let (y, p') = g p x in f p' y)
+deriving instance Functor (k a) => Functor (NonDet k a)
 
-instance Num p => ProductCat (NonDet p) where
-    exl = N (\p x -> (exl x, p))
-    exr = N (\p x -> (exr x, p))
-    N f &&& N g = N $ \p x ->
-        let (f', fp) = f p x
-            (g', gp) = g p x in
-        ((f', g'), fp + gp)
+instance Category k => Category (NonDet k) where
+    type Ok (NonDet k) = Ok k
+    id = NonDet (\() -> id)
+    NonDet f . NonDet g = NonDet $ \(p1, p2) -> f p1 . g p2
 
-instance TerminalCat (NonDet p) where
-    it = N (\p x -> (it x, p))
+instance ProductCat k => ProductCat (NonDet k) where
+    exl = NonDet (\() -> exl)
+    exr = NonDet (\() -> exr)
+    NonDet f &&& NonDet g = NonDet $ \(p1, p2) -> f p1 &&& g p2
 
-instance ConstCat (NonDet p) b where
-    const b = N (\p _ -> (b, p))
+instance  TerminalCat k => TerminalCat (NonDet k) where
+    it = NonDet (\() -> it)
 
-instance BottomCat (NonDet p) a b where
-    bottomC = N (\p x -> (bottomC x, p))
+instance  ConstCat k b => ConstCat (NonDet k) b where
+    const b = NonDet (\() -> const b)
 
-instance UnknownCat (NonDet p) a b where
-    unknownC = N (\p x -> (unknownC x, p))
+instance  BottomCat k a b => BottomCat (NonDet k) a b where
+    bottomC = NonDet (\() -> bottomC)
 
-instance Num p => ClosedCat (NonDet p) where
-    -- jww (2017-04-21): This is wrong
-    curry   (NonDet f) = N $ \p x -> ((\y -> fst (f p (x, y))), p)
-    uncurry (NonDet f) = N $ \p (x, y) -> let (f', p') = f p x in (f' y, p')
+instance  UnknownCat k a b => UnknownCat (NonDet k) a b where
+    unknownC = NonDet (\() -> unknownC)
 
-instance CoproductCat (NonDet p) where
-    inl = N (\p x -> (inl x, p))
-    inr = N (\p x -> (inr x, p))
-    N f ||| N g = error "(|||) NYI"
+instance  ClosedCat k => ClosedCat (NonDet k) where
+    curry   (NonDet f) = NonDet $ \p -> curry (f p)
+    uncurry (NonDet f) = NonDet $ \p -> uncurry (f p)
 
-instance (Num p, Eq a) => EqCat (NonDet p) a where
-    equal    = N (\p x -> (equal x, p))
-    notEqual = N (\p x -> (notEqual x, p))
+instance  CoproductCat k => CoproductCat (NonDet k) where
+    inl = NonDet (\() -> inl)
+    inr = NonDet (\() -> inr)
+    NonDet f ||| NonDet g = NonDet $ \(p1, p2) -> f p1 ||| g p2
 
-instance (Num p, Ord a) => OrdCat (NonDet p) a where
-    lessThan           = N (\p x -> (lessThan x, p))
-    greaterThan        = N (\p x -> (greaterThan x, p))
-    lessThanOrEqual    = N (\p x -> (lessThanOrEqual x, p))
-    greaterThanOrEqual = N (\p x -> (greaterThanOrEqual x, p))
+instance (EqCat k a, Eq a) => EqCat (NonDet k) a where
+    equal    = NonDet (\() -> equal)
+    notEqual = NonDet (\() -> notEqual)
 
--- instance Fractional a => FractionalCat (NonDet p) a where
---     recipC = N recip
---     divideC = N (uncurry (/))
+instance (OrdCat k a, Ord a) => OrdCat (NonDet k) a where
+    lessThan           = NonDet (\() -> lessThan)
+    greaterThan        = NonDet (\() -> greaterThan)
+    lessThanOrEqual    = NonDet (\() -> lessThanOrEqual)
+    greaterThanOrEqual = NonDet (\() -> greaterThanOrEqual)
 
--- instance (RealFrac a, Integral b) => RealFracCat (NonDet p) a b where
---     floorC = N floor
---     ceilingC = N ceiling
+instance (FractionalCat k a, Fractional a) => FractionalCat (NonDet k) a where
+    recipC  = NonDet (\() -> recipC)
+    divideC = NonDet (\() -> divideC)
 
--- instance Floating a => FloatingCat (NonDet p) a where
---     expC = N exp
---     cosC = N cos
---     sinC = N sin
+instance (RealFracCat k a b, RealFrac a, Integral b) => RealFracCat (NonDet k) a b where
+    floorC = NonDet (\() -> floorC)
+    ceilingC = NonDet (\() -> ceilingC)
 
--- instance (Integral a, Num b) => FromIntegralCat (NonDet p) a b where
---     fromIntegralC = N fromIntegral
+instance (FloatingCat k a, Floating a) => FloatingCat (NonDet k) a where
+    expC = NonDet (\() -> expC)
+    cosC = NonDet (\() -> cosC)
+    sinC = NonDet (\() -> sinC)
 
--- instance DistribCat (NonDet p) where
---     distl = N $ \(x, e) -> case e of
---         Left y  -> Left (x, y)
---         Right z -> Right (x, z)
---     distr = N $ \(e, x) -> case e of
---         Left y  -> Left (y, x)
---         Right z -> Right (z, x)
+instance (FromIntegralCat k a b, Integral a, Num b) => FromIntegralCat (NonDet k) a b where
+    fromIntegralC = NonDet (\() -> fromIntegralC)
 
-instance (HasRep a, r ~ Rep a) => RepCat (NonDet p) a r where
-    reprC = N (\p x -> (repr x, p))
-    abstC = N (\p x -> (abst x, p))
+instance (DistribCat k) => DistribCat (NonDet k) where
+    distl = NonDet (\() -> distl)
+    distr = NonDet (\() -> distr)
 
-instance (Enum a, Show a) => EnumCat (NonDet p) a where
-    succC = N (\p x -> (succ x, p))
-    predC = N (\p x -> (pred x, p))
+instance RepCat k a r => RepCat (NonDet k) a r where
+    reprC = NonDet (\() -> reprC)
+    abstC = NonDet (\() -> abstC)
 
-instance Num p => BoolCat (NonDet p) where
-    notC = N (\p x -> (notC x, p))
-    andC = N (\p x -> (andC x, p))
-    orC  = N (\p x -> (orC x, p))
-    xorC = N (\p x -> (xorC x, p))
+instance (EnumCat k a, Enum a) => EnumCat (NonDet k) a where
+    succC = NonDet (\() -> succC)
+    predC = NonDet (\() -> predC)
 
-instance Num p => IfCat (NonDet p) a where
-    ifC = N (\p x -> (ifC x, p))
+instance BoolCat k => BoolCat (NonDet k) where
+    notC = NonDet (\() -> notC)
+    andC = NonDet (\() -> andC)
+    orC  = NonDet (\() -> orC)
+    xorC = NonDet (\() -> xorC)
 
-instance Num a => NumCat (NonDet p) a where
-    negateC = N (\p x -> (negateC x, p))
-    addC    = N (\p x -> (addC x, p))
-    subC    = N (\p x -> (subC x, p))
-    mulC    = N (\p x -> (mulC x, p))
-    powIC   = N (\p x -> (powIC x, p))
+instance  IfCat k a => IfCat (NonDet k) a where
+    ifC = NonDet (\() -> ifC)
 
-instance ProgramCat (NonDet Int) Int where
-    xfer = N (\l x -> (if l < 10 then xfer x else xfer x, l + 5))
-    add  = N (\l x -> (if l < 10 then add  x else add  x, l + 50))
-    mov  = N (\l x -> (if l < 10 then mov  x else mov  x, l + 30))
-    load = N (\l x -> (if l < 10 then load x else load x, l + 10))
-    ret  = N (\l x -> (if l < 10 then ret  x else ret  x, l + 1))
+instance (NumCat k a, Num a) => NumCat (NonDet k) a where
+    negateC = NonDet (\() -> negateC)
+    addC    = NonDet (\() -> addC)
+    subC    = NonDet (\() -> subC)
+    mulC    = NonDet (\() -> mulC)
+    powIC   = NonDet (\() -> powIC)
+
+instance ProgramCat k Int => ProgramCat (NonDet k) Int where
+    xfer = NonDet (\(l :: Int) -> (if l < 10 then xfer else xfer))
+    add  = NonDet (\(l :: Int) -> (if l < 10 then add  else add))
+    mov  = NonDet (\(l :: Int) -> (if l < 10 then mov  else mov))
+    load = NonDet (\(l :: Int) -> (if l < 10 then load else load))
+    ret  = NonDet (\(l :: Int) -> (if l < 10 then ret  else ret))
